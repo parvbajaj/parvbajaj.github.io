5970f8ad823eb551416c84c631e8dc20241103c7
Parv Bajaj <parvbajaj2000@gmail.com>
2025-03-29 19:57:59 +0530

    Add .DS_Store and update index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,6 +91,19 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("load", toggleScrollButtons);
 });
 
+/**
+ * Animation on scroll function and init
+ */
+function aosInit() {
+    AOS.init({
+        duration: 600,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+    });
+}
+window.addEventListener('load', aosInit);
+
   /**
    * Init typed.js
    */
